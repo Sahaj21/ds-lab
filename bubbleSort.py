19911919dcd6e58bcdfa9076feced9d6383a11d7
aldrin bautista <adinbautista@gmail.com>
2018-10-06 17:58:30 +0800

    for #1 optimize bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,13 +1,14 @@
 print("Enter the array to be sorted \n")
 a = [int(i) for i in input().split()]
 
-for i in range(len(a)-1):
-	for j in range(len(a)-1):
-		if a[j] > a[j+1]:
-			# swap= ar[j];
-			# ar[j] =ar[j+1];
-			# ar[j+1]= swap;
-			a[j], a[j+1] = a[j+1] , a[j];
-
+swapped = True
+for i in range(len(a)-1, 0, -1):
+        if not swapped:
+                break
+        swapped = False
+        for j in range(i):
+                if a[j] > a[j+1]:
+                        a[j], a[j+1] = a[j+1] , a[j]
+                        swapped = True
 
 print("Acending Order Sorted Array" ,a)
