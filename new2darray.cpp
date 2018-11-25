ccd4e53f494cd82347d5e48e4ef42e4b921ed6cf
Sahaj Kulshrestha <31099896+Sahaj21@users.noreply.github.com>
2018-11-25 11:59:41 +0530

    2d array

---
+++ new2darray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int **p=new int*[3];
+
+for(int i=0;i<3;i++)
+p[i]=new int[3];
+
+  for(int i=0;i<3;i++)
+  for(int j=0;j<3;j++)
+  cin>>p[i][j];
+  
+  for(int i=0;i<3;i++)
+  {
+  	
+  for(int j=0;j<3;j++)
+  cout<<p[i][j]<<" ";
+  
+  cout<<endl;
+  }
+}
+
